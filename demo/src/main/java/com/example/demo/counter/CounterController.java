b54bd1eeb5e1139e9939253a851773abe5e5353d
Anh Tuan <ntuaans13@gmail.com>
2025-12-02 09:12:48 +0700

    update

--- demo/src/main/java/com/example/demo/counter/CounterController.java
+++ demo/src/main/java/com/example/demo/counter/CounterController.java
@@ -6,34 +6,33 @@ import org.springframework.web.bind.annotation.RestController;
 
 import lombok.extern.slf4j.Slf4j;
 
-@slf4j
+@Slf4j
 @RestController
 @RequestMapping("/api/counter")
 
 public class CounterController {
+
     private final CounterService counterService;
-    
+
     public CounterController(CounterService counterService) {
         this.counterService = counterService;
     }
 
     @GetMapping("/reset")
-    public string resetCounter() {
+    public String resetCounter() {
         counterService.resetCounter();
-        return "Counter value: " + this.getCounter();
+        return "Current counter value: " + counterService.getCounter();
     }
 
     @GetMapping("/increment")
     public String incrementCounter() {
         int newCounterValue = counterService.increment();
-        log.info("newCounterValue: " + newCounterValue);
+        log.info("newCounterValue: {}", newCounterValue);
         return "Counter value: " + newCounterValue;
     }
-
 
     @GetMapping("/value")
     public String getCounter() {
         return "Current counter value: " + counterService.getCounter();
     }
-
 }
--- demo/src/main/java/com/example/demo/counter/CounterService.java
+++ demo/src/main/java/com/example/demo/counter/CounterService.java
@@ -6,6 +6,7 @@ import org.springframework.stereotype.Service;
 
 @Service
 public class CounterService {
+
     private final AtomicInteger counter = new AtomicInteger(0);
 
     public int increment() {
